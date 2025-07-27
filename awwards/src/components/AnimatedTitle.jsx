f7a2f131e4d5043ee09e53043039a42e4084c2b9
DhruvP2006 <dhruv.pankhania@somaiya.edu>
2025-07-27 21:08:07 +0530

    story section

--- awwards/src/components/AnimatedTitle.jsx
+++ awwards/src/components/AnimatedTitle.jsx
@@ -1,6 +1,6 @@
-import React, { useEffect, useRef } from "react";
-import gsap from "gsap/all";
-import { ScrollTrigger } from "gsap/all";
+import { gsap } from "gsap";
+import { useEffect, useRef } from "react";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import clsx from "clsx";
 
 gsap.registerPlugin(ScrollTrigger);
@@ -15,30 +15,35 @@ const AnimatedTitle = ({ title, containerClass }) => {
           trigger: containerRef.current,
           start: "100 bottom",
           end: "center bottom",
-          toggleActions: "play none none reverse ",
+          toggleActions: "play none none reverse",
         },
       });
 
-      titleAnimation.to(".animated-word", {
-        opacity: 1,
-        transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
-        ease: "power2.inOut",
-        stagger: 0.02,
-      });
+      titleAnimation.to(
+        ".animated-word",
+        {
+          opacity: 1,
+          transform: "translate3d(0, 0, 0) rotateY(0deg) rotateX(0deg)",
+          ease: "power2.inOut",
+          stagger: 0.02,
+        },
+        0
+      );
     }, containerRef);
-    return () => ctx.revert();
+
+    return () => ctx.revert(); // Clean up on unmount
   }, []);
 
   return (
-    <div ref={containerRef} className={`animated-title ${containerClass}`}>
+    <div ref={containerRef} className={clsx("animated-title", containerClass)}>
       {title.split("<br />").map((line, index) => (
         <div
           key={index}
-          className="felx-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
+          className="flex-center max-w-full flex-wrap gap-2 px-10 md:gap-3"
         >
-          {line.split(" ").map((word, i) => (
+          {line.split(" ").map((word, idx) => (
             <span
-              key={i}
+              key={idx}
               className="animated-word"
               dangerouslySetInnerHTML={{ __html: word }}
             />
